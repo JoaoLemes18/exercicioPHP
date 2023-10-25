8c3a1d82fb15c93b84d189aff695379a5b935ecd
JoaoLemes18 <joaolemes17@hotmail.com>
2023-10-25 19:58:56 -0400

    add

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Formulário</h1>
+
+<form action="/action_page.php">
+  <label for="fname">Seu Nome:</label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+  <label for="lname">Ultimo nome:</label><br>
+  <input type="text" id="lname" name="lname" value=""><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+<?php
+
+?>
+
+</body>
+</html>
